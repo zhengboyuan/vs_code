b956d7c4f9f552b3eb766c483a043ddf98fda87c
813925044@qq.com <813925044@qq.com>
2024-08-20 09:41:32 +0800

    This commit introduces the 'data.py' module which contains the initial dataset for our project.

---
+++ project/data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+print(df)
+df.set_index("col1", inplace=True, drop=False)
+print(
+    (
+        df.assign(col3=lambda x: x.col1 + x.col2)
+        .assign(col4=lambda x: -x.col3 + 1)
+        .assign(col5=lambda x: x.col4 + x.col3)
+    )
+)
+
+
+def fuc(a, b):
+    c = a + b
+    c = c
+    return c
+
+
+fuc(4, 1)
